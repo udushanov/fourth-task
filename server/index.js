03de34e842335c285af2fad171d83e00ad7f4829
Ulugbek <u.dushanov@gmail.com>
2023-03-17 06:22:26 +0500

    feat: add server routes and connection to mysql

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import mysql from 'mysql2'
+import authRoutes from './routes/auth.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/auth', authRoutes)
+
+export const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '5130921a',
+  database: ''
+})
+
+app.listen(8080, () => {
+  console.log('The server is running')
+})
