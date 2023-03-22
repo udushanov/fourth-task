a64de40c6d440bb0a10ac20725e1664376d8e506
Ulugbek <u.dushanov@gmail.com>
2023-03-22 12:46:55 +0500

    docs: update.env file variables

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "5130921a",
-  database: "users",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 app.get("/", (req, res) => {
